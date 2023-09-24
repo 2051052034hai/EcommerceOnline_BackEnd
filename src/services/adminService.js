aac140e729c85aa1da1b5b61136e4abe91b6a157
vothanhtinh <vothanhtinh147@gmail.com>
2023-09-24 18:57:15 +0700

    chore(adminService): fix import

--- src/services/adminService.js
+++ src/services/adminService.js
@@ -1,53 +1,54 @@
-import aqp from "api-query-params";
 import User from "../models/User";
+import bcrypt from "bcrypt";
+import { genneralAccessToken, genneralRefreshToken } from "./jwtService";
 
-const  adminService= {
-    loginUser: async (userLogin) => {
-        const { email, password } = userLogin;
-    
-        try {
-          const user = await User.findOne({
-            email: email,
-            role:3
-          });
-          if (user === null) {
-            return {
-              status: "ERR",
-              message: "The user is not defined",
-            };
-          }
-          const comparePassword = bcrypt.compareSync(
-            userLogin.password,
-            user.password
-          );
-    
-          if (!comparePassword) {
-            return {
-              status: "ERR",
-              message: "The password or user is incorrect",
-            };
-          
-          }
-    
-          const access_token = await genneralAccessToken({
-            id: user.id,
-            role: user.role,
-          });
-    
-          const refresh_token = await genneralRefreshToken({
-            id: user.id,
-            role: user.role,
-          });
-    
-          // Trả về user sau khi đã bỏ password
-          const { password, ...other } = user._doc;
-          const newUserUpdate = Object.assign({}, other);
-    
-          return { ...newUserUpdate, access_token, refresh_token };
-        } catch (e) {
-          return e;
-        }
-      },
-}
+const adminService = {
+  loginUser: async (userLogin) => {
+    console.log(userLogin);
+    const { email, password } = userLogin;
 
-export default adminService
+    try {
+      const user = await User.findOne({
+        email: email,
+        role: 3,
+      });
+      if (user === null) {
+        return {
+          status: "ERR",
+          message: "The user is not defined",
+        };
+      }
+      const comparePassword = bcrypt.compareSync(
+        userLogin.password,
+        user.password
+      );
+
+      if (!comparePassword) {
+        return {
+          status: "ERR",
+          message: "The password or user is incorrect",
+        };
+      }
+
+      const access_token = await genneralAccessToken({
+        id: user.id,
+        role: user.role,
+      });
+
+      const refresh_token = await genneralRefreshToken({
+        id: user.id,
+        role: user.role,
+      });
+
+      // Trả về user sau khi đã bỏ password
+      const { password, ...other } = user._doc;
+      const newUserUpdate = Object.assign({}, other);
+
+      return { ...newUserUpdate, access_token, refresh_token };
+    } catch (e) {
+      return e;
+    }
+  },
+};
+
+export default adminService;
