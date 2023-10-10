6b19997c023da8954175e56cedc6520cefe0bfe1
vothanhtinh <vothanhtinh147@gmail.com>
2023-10-10 17:13:25 +0700

    chore: change cors

--- src/server.js
+++ src/server.js
@@ -20,16 +20,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
-  );
-  next();
-});
+app.use(cors());
 
 app.use(bodyParser.json()); // for json
 app.use(bodyParser.urlencoded({ extended: true }));
