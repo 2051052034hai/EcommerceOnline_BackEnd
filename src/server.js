68a020d156b585cc50b2f7d105bd468da08c6ac1
vothanhtinh <vothanhtinh147@gmail.com>
2023-10-10 21:22:27 +0700

    change cors

--- src/server.js
+++ src/server.js
@@ -19,8 +19,13 @@ import vnpayAPI from "./routers/vnpayAPI";
 dotenv.config();
 
 const app = express();
-
-app.use(cors());
+app.use(
+  cors({
+    origin: "ht-shop-three.vercel.app",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // Allow cookies and other credentials
+  })
+);
 
 app.use(bodyParser.json()); // for json
 app.use(bodyParser.urlencoded({ extended: true }));
