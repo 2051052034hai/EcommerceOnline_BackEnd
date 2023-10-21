2d6c7284adb6b9f8985853d5e08246fd9dc98d6e
vothanhtinh <vothanhtinh147@gmail.com>
2023-10-21 16:04:13 +0700

    chore: change expriresIn JWT

--- src/services/jwtService.js
+++ src/services/jwtService.js
@@ -8,7 +8,7 @@ export const genneralAccessToken = async (payload) => {
       ...payload,
     },
     process.env.JWT_ACCESS_KEY,
-    { expiresIn: "30s" }
+    { expiresIn: "30day" }
   );
 
   return access_token;
